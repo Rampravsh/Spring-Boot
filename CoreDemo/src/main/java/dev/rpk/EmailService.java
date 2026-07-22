f6a2321f17301bfbf6c5cd32d0e1c369e056d5d7
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-07-22 16:24:02 +0530

    somechanges

---
+++ CoreDemo/src/main/java/dev/rpk/EmailService.java
@@ -0,0 +1,4 @@
+package dev.rpk;
+
+public class EmailService {
+}
---
+++ CoreDemo/src/main/java/dev/rpk/OrderService.java
@@ -0,0 +1,8 @@
+package dev.rpk;
+
+
+public class OrderService {
+    public void placeOrder(){
+        System.out.println("order placed");
+    }
+}
