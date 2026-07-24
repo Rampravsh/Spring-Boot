13175ad718af57e7579c3cafe6d06297ab80976d
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-07-24 16:31:58 +0530

    all file

---
+++ BeanInitializationDemo/src/main/java/dev/rpk/AppConfig.java
@@ -0,0 +1,10 @@
+package dev.rpk;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.rpk")
+public class AppConfig {
+}
